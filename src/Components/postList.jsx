0fc8afb6cf7ad36c4b87feae3870f2d2b88da9aa
samdragneal <samdragneal@gmail.com>
2020-07-26 22:35:15 +0530

    button for single post

--- src/Components/postList.jsx
+++ src/Components/postList.jsx
@@ -1,19 +1,34 @@
 import React,{ useState, useEffect  } from "react";
-// import axios from "axios";
+import {Link} from "react-router-dom";
+
+//function for getting year
+function renderDate(dateString){
+  const renderMonth = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+  const date = new Date(dateString);
+  return renderMonth[date.getMonth()] +" " + date.getDate()+" " + date.getFullYear();
+
+
+}
+//render tags
+
+function renderTags(tags){
+  return tags.map((tag)=>{
+    return (<span key = {tag}> {tag} </span>)
+  })
+}
 
 function PostList(){
 
-  //
-  //
+  //using hooks to fetch data
 
-  //
-  //
 const [posts,setPosts] = useState([]);
 
 useEffect(() => {
    getPosts();
  }, []);
 
+//using async and await to get promise
 const getPosts = async ()=>{
   // const res = await axios.get("http://localhost:3000/posts/");
   const data = await fetch(
@@ -23,10 +38,19 @@ const getPosts = async ()=>{
   setPosts(set);
 
 };
+
   return (
     <div>
     {posts.map(post => (
-        <p>{post.html}</p>
+      <Link to = {"/posts/" + post._id}>
+      <button key = {post._id}>
+        <h1>{post.title}</h1>
+        <span>{ renderDate(post.createdAt)}
+        </span>
+
+        <div>{renderTags(post.tags)}</div>
+        </button>
+        </Link>
       ))}
     </div>
   )
