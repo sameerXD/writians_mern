8c37d3e54856a52f1d09f84e8eda85aaeb6ecd43
samdragneal <samdragneal@gmail.com>
2020-07-28 18:11:51 +0530

    show post by id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {BrowserRouter,Switch,Route} from "react-router-dom";
+//pages
 import PostList from "./Components/postList";
+import Post from "./Components/Post";
 
 function App(){
   return(
     <BrowserRouter>
     <Switch>
+    <Route path ="/posts/:id" component={Post}/>
     <Route path ="/posts" component={PostList}/>
+
     </Switch>
     </BrowserRouter>
   )
---
+++ src/Components/Post.jsx
@@ -0,0 +1,31 @@
+import React,{useState,useEffect} from "react";
+
+
+function Post(props){
+  const [post,setPost] = useState({});
+  const tik =[1,2,3]
+  useEffect(()=>{
+    getPosts();
+  },[]);
+
+  const getPosts = async ()=>{
+    const data = await fetch("http://localhost:5000/posts/"+props.match.params.id);
+
+    const set = await data.json();
+    setPost(set);
+  }
+  function renderHtml(){
+    return {__html:post.html}
+  }
+  function renderPost(){
+    return <div dangerouslySetInnerHTML={renderHtml()}></div>;
+  }
+
+  return(
+    <div>
+     {renderPost()}
+    </div>
+  )
+};
+
+export default Post;
